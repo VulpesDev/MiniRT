20b64ca4ebe9ee094f57eb48e6739e50aaea4f50
Timoti Fregni <timotif@gmail.com>
2023-08-29 00:00:40 +0200

    added matrix and substituted top_left to bottom_left

--- includes/camera.h
+++ includes/camera.h
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 12:39:08 by tfregni           #+#    #+#             */
-/*   Updated: 2023/06/16 15:35:24 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/08/28 23:51:28 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdint.h>
 # include <math.h>
 # include "vec3.h"
+# include "matrix_math.h"
 
 # define WIDTH 1024
 # define HEIGHT 768
@@ -25,15 +26,17 @@ typedef struct s_camera
 	t_vec3			orientation;
 	t_point3		look_at;
 	uint8_t			fov;
+	double			aspect_ratio;
 	double			viewport_height;
 	double			viewport_width;
 	double			focal_length;
-	t_point3		lower_left_corner;
+	t_point3		top_left_corner;
 	t_vec3			horizontal;
 	t_vec3			vertical;
 	t_vec3			vup;
 	t_vec3			up;
 	t_vec3			right;
+	t_matrix		m;
 }					t_camera;
 
 void		cam_setup(t_camera *c);
