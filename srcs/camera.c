c5411647b9e76169024fc5aa95b2903435a9977a
Timoti Fregni <timotif@gmail.com>
2023-08-28 23:59:32 +0200

    reworked camera orientation and top_left position to match pixel 0,0

--- srcs/camera.c
+++ srcs/camera.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 12:54:25 by tfregni           #+#    #+#             */
-/*   Updated: 2023/06/16 17:25:39 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/08/28 23:54:29 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,48 +20,55 @@ double	radians(double grades)
 
 void	cam_orientation(t_camera *c)
 {
-	t_point3	lookat;
-	// t_vec3		w;
-	// t_vec3		u;
-	// t_vec3		v;
-
-	lookat = cam_look_at(c->look_at, c->orientation);
-	printf("LookAt: %f %f %f\n", lookat.x, lookat.y, lookat.z);
-	// w = vec3_unit(vec3_sub(c->pos, c->lookat))
+	c->right = vec3_unit(vec3_cross(c->orientation, c->vup));
+	c->up = vec3_unit(vec3_cross(c->right, c->orientation));
+	c->look_at = vec3_unit(c->orientation);
+	c->m.size = 16;
+	c->m.matrix[0][0] = c->right.x;
+	c->m.matrix[0][1] = c->right.y;
+	c->m.matrix[0][2] = c->right.z;
+	c->m.matrix[0][3] = 0;
+	c->m.matrix[1][0] = c->up.x;
+	c->m.matrix[1][1] = c->up.y;
+	c->m.matrix[1][2] = c->up.z;
+	c->m.matrix[1][3] = 0;
+	c->m.matrix[2][0] = c->vup.x;
+	c->m.matrix[2][1] = c->vup.y;
+	c->m.matrix[2][2] = c->vup.z;
+	c->m.matrix[2][3] = 0;
+	c->m.matrix[3][0] = c->pos.x;
+	c->m.matrix[3][1] = c->pos.y;
+	c->m.matrix[3][2] = c->pos.z;
+	c->m.matrix[3][3] = 1;
 }
 
 void	cam_setup(t_camera *c)
 {
-	double	ratio;
 	t_vec3	half_hor;
 	t_vec3	half_ver;
 	t_vec3	temp;
 
 	cam_orientation(c);
-	ratio = WIDTH / (double)HEIGHT;
+	c->aspect_ratio = WIDTH / (double)HEIGHT;
 	c->viewport_width = 2.0f * (tan(radians(c->fov) / 2.0f));
-	c->viewport_height = c->viewport_width / ratio;
+	c->viewport_height = c->viewport_width / c->aspect_ratio;
 	c->horizontal = vec3(c->viewport_width, 0, 0);
 	c->vertical = vec3(0, c->viewport_height, 0);
 	half_hor = vec3_div(c->horizontal, 2);
 	half_ver = vec3_div(c->vertical, 2);
 	temp = vec3_sub(c->pos, half_hor);
-	temp = vec3_sub(temp, half_ver);
-	c->lower_left_corner = vec3_sub(temp, vec3(0, 0, c->focal_length));
-	printf("Camera: vp_w: %f, vp_h: %f, lower_left: %f %f %f\n", c->viewport_width, c->viewport_height, c->lower_left_corner.x, c->lower_left_corner.y, c->lower_left_corner.z);
+	temp = vec3_sum(temp, half_ver);
+	c->top_left_corner = vec3_sub(temp, vec3(0, 0, c->focal_length));
+	printf("Camera: vp_w: %f, vp_h: %f, ratio: %f, top_left: %f %f %f\n", c->viewport_width, c->viewport_height, c->aspect_ratio, c->top_left_corner.x, c->top_left_corner.y, c->top_left_corner.z);
+	printf("Horizontal: ");
+	vec3_print(c->horizontal);
+	printf(" Half_hor: ");
+	vec3_print(half_hor);
+	printf("\nVertical: ");
+	vec3_print(c->vertical);
+	printf(" Half_ver: ");
+	vec3_print(half_ver);
+	printf("\n");
 }
 
-/**
- * @brief Given the forward vector and a vector with 3 rotation angles
- * in range [-1, 1], it gives the look_at point
-*/
-t_point3	cam_look_at(t_vec3 forward, t_vec3 rot)
-{
-	t_vec3	look_at;
-
-	look_at.x = forward.x * cos(rot.x * M_PI) * cos(rot.y * M_PI);
-	look_at.y = forward.y * sin(rot.x * M_PI) * cos(rot.y * M_PI);
-	look_at.z = forward.z * sin(rot.y * M_PI);
-	return (vec3_unit(look_at));
-}
 
