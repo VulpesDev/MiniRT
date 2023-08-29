24fb3ff6a1f9c1d8cc98f8ad17bd6085f0750d4a
Timoti Fregni <timotif@gmail.com>
2023-08-29 10:31:28 +0200

    fixed light direction in diffuse function

--- srcs/shade.c
+++ srcs/shade.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 15:45:15 by tfregni           #+#    #+#             */
-/*   Updated: 2023/08/29 10:17:10 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/08/29 10:30:05 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	cast_shadow(t_scene *scene, t_ray ray)
  * @returns	a float expressing the light intensity on the point
  * @brief if the light hits at an angle it's more diffused and
  * less intense
- * @param l: light ray
+ * @param l: light ray (from the hit point to the light source)
  * @param n_dot_l: angle between the light ray and the normal
  * @param light: intensity of the light. If it's negative it's
  * set to 0
@@ -47,15 +47,16 @@ float	diffuse_reflection(t_scene *scene, t_vector n, t_vector p)
 	float		len_l;
 	t_ray		rebound;
 
-	l = vec3_sub(p, scene->light.pos);
-	rebound = (t_ray){p, vec3_inv(l)};
+	l = vec3_sub(scene->light.pos, p);
+	rebound = (t_ray){p, l};
 	if (cast_shadow(scene, rebound))
 		return (0.1f);
 	n_dot_l = vec3_dot(n, l);
 	len_l = vec3_len(l);
 	if (len_l < EPSILON)
 		return (1.0f);
-	light = ft_fmax((scene->light.brightness * n_dot_l / len_l) + scene->ambient.lighting_ratio, 0.0f);
+	light = ft_fmax((scene->light.brightness * n_dot_l / len_l) \
+		+ scene->ambient.lighting_ratio, 0.0f);
 	return (light);
 }
 
