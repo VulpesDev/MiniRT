4b3c720e59875476fd6683bb72d2d5a28445c64b
tim <timotif@gmail.com>
2023-09-21 10:31:23 +0200

    added comment to ray function

--- srcs/ray.c
+++ srcs/ray.c
@@ -6,12 +6,15 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 12:14:47 by tfregni           #+#    #+#             */
-/*   Updated: 2023/09/12 12:21:37 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/09/21 10:30:40 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray.h"
 
+/**
+ * @brief Create a ray object
+*/
 t_ray	ray(t_point3 origin, t_vec3 direction)
 {
 	t_ray	result;
