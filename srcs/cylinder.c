e27885dceed3a9cee55bce604502c0182e755ec2
tim <timotif@gmail.com>
2023-09-22 13:11:21 +0200

    separated the normal calculation to its own function

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:21:29 by tfregni           #+#    #+#             */
-/*   Updated: 2023/09/21 09:52:45 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/09/22 13:10:19 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,29 @@
 /**
  * @explanation: to find the normal I find the distance along
  * the center vector at the level of the hit point:
- * double center_t = vec3_dot(hit_vec, shape->cy.vec);
+ * double center_t = vec3_dot(hit_vec, cy->cy.vec);
  * Then the coordinate of the point on the center vector is:
- * rec->normal = vec3_sum(shape->cy.top, vec3_mult(shape->cy.vec, center_t));
+ * normal = vec3_sum(cy->cy.top, vec3_mult(cy->cy.vec, center_t));
  * Finally, the normal is the normalized vector from the hit point to
  * the point I found along the center vector.
 */
+t_vec3	cy_normal(t_shape *cy, t_point_3d hit)
+{
+	double	center_t;
+	t_vec3	hit_vec;
+	t_vec3	normal;
+
+	hit_vec = vec3_sub(hit, cy->cy.top);
+	center_t = vec3_dot(hit_vec, cy->cy.vec);
+	normal = vec3_sum(cy->cy.top, \
+		vec3_mult(cy->cy.vec, center_t));
+	normal = vec3_unit(vec3_sub(hit, normal));
+	return (normal);
+}
+
 bool	cy_hit_record(double t, t_shape *shape, t_hit_record *rec, t_ray ray)
 {
 	t_vec3	hit_vec;
-	double	center_t;
 
 	hit_vec = vec3_sub(ray_at(ray, t), shape->cy.top);
 	if (t > EPSILON && t < rec->t && vec3_dot(hit_vec, shape->cy.vec) >= 0
@@ -35,10 +48,7 @@ bool	cy_hit_record(double t, t_shape *shape, t_hit_record *rec, t_ray ray)
 		rec->t = t;
 		rec->p = ray_at(ray, t);
 		rec->shape = shape;
-		center_t = vec3_dot(hit_vec, shape->cy.vec);
-		rec->normal = vec3_sum(shape->cy.top, \
-			vec3_mult(shape->cy.vec, center_t));
-		rec->normal = vec3_unit(vec3_sub(rec->p, rec->normal));
+		rec->normal = cy_normal(shape, rec->p);
 		rec->color = shape->color;
 		return (true);
 	}
