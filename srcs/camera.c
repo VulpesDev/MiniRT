77204ec95a3a2771c209a18b48dbd56ae162a7af
timotif <timotif@gmail.com>
2023-09-23 17:50:58 +0200

    rolled back to previous camera and selected different test scenes

--- srcs/camera.c
+++ srcs/camera.c
@@ -6,7 +6,7 @@
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 12:54:25 by tfregni           #+#    #+#             */
-/*   Updated: 2023/09/21 09:45:18 by tfregni          ###   ########.fr       */
+/*   Updated: 2023/09/23 17:48:58 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,29 @@ double	radians(double grades)
  * - the orientation vector gives the coordinates of the point the camera
  * is looking at (orient (0, 0, 0) means that the camera is looking at (0, 0, 0))
  * wherever the camera is.
- * We chose the second option. Here in comment the implementation of the first.
-// if (vec3_len_squared(c->orientation) == 0)
-// 	c->orientation = vec3(0, 0, -1);
-// c->orientation = vec3_unit(c->orientation);
-// c->look_at = vec3_sum(c->pos, c->orientation);
-// c->look_at = vec3_sub(c->look_at, c->pos);
- * @explanation:
+ * We chose the first option. Here in comment the implementation of the second.
+ * // c->look_at = vec3_unit(vec3_sub(c->orientation, c->pos));
  * - calculate the look_at vector as direction between position and orient
+ * @explanation:
+ * - check that the orientation vector is not 0, 0, 0 before trying to normalize
+ * - translate the orientation vector to the camera position
+ * - compute the direction from position to translated orientation
  * - find the right vector through the cross product with the vertical up
  * - if the right vector is 0 (camera along y axis) calculate it with another
- * arbitrary vector (1, 0, 0)
+ * arbitrary vector (0, 0, -1)
  * - normalize the right vector
  * - find the up vector through the cross product with the right vector
 */
 void	cam_orientation(t_camera *c)
 {
-	c->look_at = vec3_unit(vec3_sub(c->orientation, c->pos));
+	if (vec3_len_squared(c->orientation) == 0)
+		c->orientation = vec3(0, 0, -1);
+	c->orientation = vec3_unit(c->orientation);
+	c->look_at = vec3_sum(c->pos, c->orientation);
+	c->look_at = vec3_sub(c->look_at, c->pos);
 	c->right = vec3_cross(c->look_at, c->vert_up);
 	if (vec3_len_squared(c->right) < EPSILON)
-		c->right = vec3_cross(c->look_at, vec3(1, 0, 0));
+		c->right = vec3_cross(c->look_at, vec3(0, 0, -1));
 	c->right = vec3_unit(c->right);
 	c->up = vec3_unit(vec3_cross(c->right, c->look_at));
 }
